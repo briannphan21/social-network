0d46b3562a863a1f03ae68ccf895619cafb45fbd
Brian Phan <briannphan21@gmail.com>
2020-03-05 19:13:34 -0500

    User API Routes and JWT Authentication

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-// @route   GET api/posts
+// @route   GET api/profile
 // @desc    Test route
 // @access  Public
-router.get('/', (req, res) => res.send('Post route'));
+router.get('/', (req, res) => res.send('Profile route'));
 
 module.exports = router;
